8a5c643128eaa2a004566ae6998dce36959e64dd
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-08-06 14:59:14 +0530

    added into the dom and mini projects

--- day8.js
+++ day8.js
@@ -25,9 +25,83 @@
 // document.body.style.backgroundColor="yellow";
 
 
-const title=document.getElementById("title");
-const btn=document.getElementById("btn");
+// const title=document.getElementById("title");
+// const btn=document.getElementById("btn");
 
-btn.addEventListener("click",function(){
-    title.textContent="Welcome to Amazon!";
+// btn.addEventListener("click",function(){
+//     title.textContent="Welcome to Amazon!";
+// })
+
+
+const cartBtn=document.getElementById("cartBtn");
+
+cartBtn.addEventListener("click",function(){
+    console.log("Product added");
+    
+})
+
+const likeBtn=document.getElementById("likeBtn");
+likeBtn.addEventListener("click",function(){
+   if (likeBtn.innerText==="🤍"){
+    likeBtn.innerText="❤️";
+   }
+   else{
+    likeBtn.innerText="🤍";
+   }
+})
+
+
+const mode=document.getElementById("mode");
+mode.addEventListener("click",function(){
+    document.body.style.backgroundColor="black";
+    document.body.style.color="white";
+})
+
+
+const login=document.getElementById("login");
+login.addEventListener("click",function(){
+    let email=document.getElementById("email").value;
+    if(email===""){
+        alert("enter email")
+    }
+    else{
+        alert("Login")
+    }
+});
+
+
+// Mini Project 1
+let count=0;
+const countText=document.getElementById("count");
+const cart=document.getElementById("cart");
+
+cart.addEventListener("click",function(){
+    count++;
+    countText.innerText=count;
+});
+
+
+// Mini Project 2
+// Live Character Counter
+const message=document.getElementById("message");
+const total=document.getElementById("total");
+
+message.addEventListener("input",function(){
+    total.innerText=message.value.length+" Characters";
+})
+
+
+
+const password=document.getElementById("password");
+const toggle=document.getElementById("toggle");
+
+toggle.addEventListener("click",function(){
+    if(password.type==="password"){
+        password.type="text";
+        toggle.innerText="Hide"
+    }
+    else{
+        password.type="password";
+        toggle.innerText="show"
+    }
 })
